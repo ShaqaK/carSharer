8c63c523a36ab28d4580139e86450b8222d08d96
amrmancy <82979516+amrmancy@users.noreply.github.com>
2022-01-27 22:28:39 +0100

    Update schreiben.java

--- src/main/java/de/unidue/inf/is/domain/schreiben.java
+++ src/main/java/de/unidue/inf/is/domain/schreiben.java
@@ -1,36 +1,42 @@
 package de.unidue.inf.is.domain;
 
 public class schreiben {
-	/* CREATE TABLE schreiben (
-  	benutzer SMALLINT NOT NULL,
-  	fahrt SMALLINT NOT NULL,
-  	bewertung SMALLINT NOT NULL,
-  	PRIMARY KEY (benutzer, fahrt),
-  	FOREIGN KEY (benutzer) REFERENCES benutzer(bid),
-  	FOREIGN KEY (fahrt) REFERENCES fahrt(fid),
-  	FOREIGN KEY (bewertung) REFERENCES bewertung(beid)
-);
 
- */
-	private int benutzer;
-	private int fahrt;
-	private int bewertung;
-	public int getBenutzer() {
-		return benutzer;
-	}
-	public void setBenutzer(int benutzer) {
-		this.benutzer = benutzer;
-	}
-	public int getFahrt() {
-		return fahrt;
-	}
-	public void setFahrt(int fahrt) {
-		this.fahrt = fahrt;
-	}
-	public int getBewertung() {
-		return bewertung;
-	}
-	public void setBewertung(int bewertung) {
-		this.bewertung = bewertung;
-	}
+    private int benutzer;
+    private int fahrt;
+    private int bewertung;
+
+    public schreiben(){
+
+    }
+
+    public schreiben(int benutzer, int fahrt, int bewertung) {
+        this.benutzer = benutzer;
+        this.fahrt = fahrt;
+        this.bewertung = bewertung;
+    }
+
+    public int getBenutzer() {
+        return benutzer;
+    }
+
+    public void setBenutzer(int benutzer) {
+        this.benutzer = benutzer;
+    }
+
+    public int getFahrt() {
+        return fahrt;
+    }
+
+    public void setFahrt(int fahrt) {
+        this.fahrt = fahrt;
+    }
+
+    public int getBewertung() {
+        return bewertung;
+    }
+
+    public void setBewertung(int bewertung) {
+        this.bewertung = bewertung;
+    }
 }
