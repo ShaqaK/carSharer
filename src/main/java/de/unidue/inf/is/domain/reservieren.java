03d08d373c8643e7f92256798ca4b6457bc51e7d
amrmancy <82979516+amrmancy@users.noreply.github.com>
2022-01-27 22:28:13 +0100

    Update reservieren.java

--- src/main/java/de/unidue/inf/is/domain/reservieren.java
+++ src/main/java/de/unidue/inf/is/domain/reservieren.java
@@ -1,35 +1,41 @@
 package de.unidue.inf.is.domain;
 
 public class reservieren {
-	/*CREATE TABLE reservieren (
-		  	kunde SMALLINT NOT NULL,
-		  	fahrt SMALLINT NOT NULL,
-		  	anzPlaetze SMALLINT NOT NULL CHECK (anzPlaetze BETWEEN 1 AND 2),
-		  	PRIMARY KEY (kunde, fahrt),
-		  	FOREIGN KEY (kunde) REFERENCES benutzer(bid),
-		  	FOREIGN KEY (fahrt) REFERENCES fahrt(fid)
-		);*/
-	public reservieren() {};
-	public int getKunde() {
-		return kunde;
-	}
-	public void setKunde(int kunde) {
-		this.kunde = kunde;
-	}
-	public int getFahrt() {
-		return fahrt;
-	}
-	public void setFahrt(int fahrt) {
-		this.fahrt = fahrt;
-	}
-	public int getAnzPlaetze() {
-		return anzPlaetze;
-	}
-	public void setAnzPlaetze(int anzPlaetze) {
-		this.anzPlaetze = anzPlaetze;
-	}
-	private int kunde;
-	private int fahrt;
-	private int anzPlaetze;
-	
+    private int kunde;
+    private int fahrt;
+    private int anzPlaetze;
+
+    public reservieren(){
+
+    }
+
+    public reservieren(int kunde, int fahrt, int anzPlaetze) {
+        this.kunde = kunde;
+        this.fahrt = fahrt;
+        this.anzPlaetze = anzPlaetze;
+    }
+
+    public int getKunde() {
+        return kunde;
+    }
+
+    public void setKunde(int kunde) {
+        this.kunde = kunde;
+    }
+
+    public int getFahrt() {
+        return fahrt;
+    }
+
+    public void setFahrt(int fahrt) {
+        this.fahrt = fahrt;
+    }
+
+    public int getAnzPlaetze() {
+        return anzPlaetze;
+    }
+
+    public void setAnzPlaetze(int anzPlaetze) {
+        this.anzPlaetze = anzPlaetze;
+    }
 }
